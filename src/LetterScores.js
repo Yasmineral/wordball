e15d0be1f9636c80abbeddf2064f6e0049b8e137
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-11-25 12:27:50 +0000

    Adds letter class and spec, passes first test

---
+++ src/LetterScores.js
@@ -0,0 +1,11 @@
+class Letters {
+  constructor () {
+    this.scores = { eaionrtlsu: 1, dgbcmp: 2, fhvwy: 3, kjx: 4, qz: 5 }
+  }
+
+  getScore () {
+    return 1
+  }
+}
+
+module.exports = Letters
