9cbfb268fd4631a1bf9b3c9b1dbeb36689c65ad1
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-11-25 15:15:43 +0000

    Randomly generates letters using a seed

--- spec/LettersSpec.js
+++ spec/LettersSpec.js
@@ -4,28 +4,42 @@ describe('LetterScores', () => {
   const letter = new Letters()
 
   describe('#getScore', () => {
-    it("Passing in 'a' as an argument returns 1", () => {
-      expect(letter.getScore('a')).toEqual(1)
+    it("Passing in 'A' as an argument returns 1", () => {
+      expect(letter.getScore('A')).toEqual(1)
     })
 
-    it("Passing in 'e' as an argument returns 1", () => {
-      expect(letter.getScore('e')).toEqual(1)
+    it("Passing in 'E' as an argument returns 1", () => {
+      expect(letter.getScore('E')).toEqual(1)
     })
 
-    it("Passing in 'b' as an argument returns 2", () => {
-      expect(letter.getScore('b')).toEqual(2)
+    it("Passing in 'B' as an argument returns 2", () => {
+      expect(letter.getScore('B')).toEqual(2)
     })
 
-    it("Passing in 'f' as an argument returns 3", () => {
-      expect(letter.getScore('f')).toEqual(3)
+    it("Passing in 'F' as an argument returns 3", () => {
+      expect(letter.getScore('F')).toEqual(3)
     })
 
-    it("Passing in 'j' as an argument returns 4", () => {
-      expect(letter.getScore('j')).toEqual(4)
+    it("Passing in 'J' as an argument returns 4", () => {
+      expect(letter.getScore('J')).toEqual(4)
     })
 
-    it("Passing in 'q' as an argument returns 5", () => {
-      expect(letter.getScore('q')).toEqual(5)
+    it("Passing in 'Q' as an argument returns 5", () => {
+      expect(letter.getScore('Q')).toEqual(5)
+    })
+  })
+
+  describe('#seededLetterGenerator', () => {
+    it('A seed of 245 and requirement of 10 will return a array of 10 fixed letters', () => {
+      const expectedArray = ['Z', 'N', 'L', 'K', 'Q', 'O', 'Z', 'B', 'T', 'D']
+      const result = letter.seededLetterGenerator(245, 10)
+      expect(result).toEqual(expectedArray)
+    })
+
+    it('A seed of 123253625 and requirement of 5 will return a array of 5 fixed letters', () => {
+      const expectedArray = ['A', 'P', 'W', 'O', 'U']
+      const result = letter.seededLetterGenerator(123253625, 5)
+      expect(result).toEqual(expectedArray)
     })
   })
 })
--- src/Letters.js
+++ src/Letters.js
@@ -1,6 +1,7 @@
 class Letters {
   constructor () {
-    this.scores = { eaionrtlsu: 1, dgbcmp: 2, fhvwy: 3, kjx: 4, qz: 5 }
+    this.scores = { EAIONRTLSU: 1, DGBCMP: 2, FHVWY: 3, KJX: 4, QZ: 5 }
+    this.letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   }
 
   getScore (letter) {
@@ -9,6 +10,23 @@ class Letters {
         return this.scores[key]
       }
     }
+  }
+
+  seededLetterGenerator (seed, required) {
+    return this.seededRand(seed, required).map((n) => {
+      return this.letters[n]
+    })
+  }
+
+  seededRand (seed, required) {
+    let i
+    const randNums = []
+    for (i = 0; i < required; i++) {
+      seed = (seed * 9301 + 49297) % 233280
+      var rnd = seed / 233280
+      randNums.push(Math.floor(0 + rnd * (26 - 0)))
+    }
+    return randNums
   }
 }
 
