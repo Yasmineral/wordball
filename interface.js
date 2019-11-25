fe01bde7d177abce72c0875928d1c12ce498ffe9
Jessie <lonsdale.jess@gmail.com>
2019-11-25 13:32:00 +0000

    started implimenting goal hole

--- interface.js
+++ interface.js
@@ -1,29 +1,67 @@
 $( document ).ready(function() {
 
+  //talk to the canvas in the html
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
+  var ball
 
   setInterval(draw, 10);
 
-  var x = 100
-  var y = 100
+  //drag and drop coordinates
+  var x1
+  var x2
+  var y1
+  var y2
+
+  $( "#canvas" ).mousedown(function(canvas) {
+    var offset = $(this).offset();
+    x1 = event.clientX-offset.left
+    y1 = event.clientY-offset.top
+  });
+
+  $( "#canvas" ).mouseup(function(canvas) {
+    var offset = $(this).offset();
+    x2 = event.clientX-offset.left
+    y2 = event.clientY-offset.top
+    ball = new Ball(x1,y1,'black',15,'A')
+    ball.giveVelocity(x1,y1,x2,y2)
+  });
+
+
 
   function draw() {
+
     //clear the board
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //draw a circle
-    drawBall(x,y)
-    //update position
-    x+=0
-    y+=10
+    ctx.beginPath();
+    ctx.rect(200, 700, 100, 100);
+    ctx.stroke();
+    //draw the ball
+    drawBall(ball)
+
+
   };
 
-  function drawBall(x,y) {
-    ctx.beginPath();
-    ctx.arc(x, y, 30, 0, 2 * Math.PI);
-    ctx.fillStyle = "#FF0000";
-    ctx.fill()
-    ctx.stroke();
+  function drawBall(ball) {
+    if(ball.isStill==false) {
+      //update position
+      ball.position()
+      x = ball.xPos
+      y = ball.yPos
+      //actually draw the ball
+      ctx.fillStyle = ball.colour;
+      ctx.beginPath();
+      ctx.arc(x, y, ball.radius, 0, 2 * Math.PI);
+      ctx.fill()
+      ctx.stroke();
+      //label the ball
+      ctx.font = "17px Arial";
+      ctx.fillStyle = "red";
+      ctx.fillText(ball.letter,x+5,y+30)
+    }
+
+
+
   };
 
 });
