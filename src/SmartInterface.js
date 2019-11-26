d5407193cf4e9999d93dbc13196e49fd86d21a08
jhmc22 <53091006+jmhc22@users.noreply.github.com>
2019-11-26 15:39:30 +0000

    Letter buttons generate based on playerLetters array

--- src/SmartInterface.js
+++ src/SmartInterface.js
@@ -1,9 +1,19 @@
 $( document ).ready(() => {
   console.log('loaded')
 
-  const playerLetters = ['A', 'C', 'P', 'K']
-
+  const playerLetters = ['D', 'O', 'N', 'E', 'S']
+  const skillPoints = 50
   const letters = new Letters()
+  const smartGame = new SmartGame(playerLetters, skillPoints)
+
+  $.get('https://jsonp.afeld.me/?url=http://anagramica.com/all/:dones', function(data) {
+      smartGame.possibleWords = data.all.filter((word) => {
+        if (word.length >= 2) { return word }
+      })
+      console.log(smartGame)
+  });
+
+  generateLetterButtons ()
 
   $('#clearbutton').click(() => {
     console.log('clearbutton')
@@ -13,5 +23,11 @@ $( document ).ready(() => {
     console.log(event.target.innerHTML)
   })
 
+  function generateLetterButtons () {
+    const buttonHTML = smartGame.playerLetters.map((letter) => {
+      return `<button class="letterbutton">${letter}</button>`
+    })
+    $('#letterkeys').html(buttonHTML.join('\n'))
+  }
 
 })
